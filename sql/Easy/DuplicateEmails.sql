41a3e74fb39dc9168dfbf257efa6d2a6d0d60e06
Yanik2 <wm_yanik@ukr.net>
2025-06-06 13:47:34 +0200

    Duplicate email

---
+++ sql/Easy/DuplicateEmails.sql
@@ -0,0 +1,21 @@
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+
+ 
+
+Write a solution to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+select distinct p1.email as Email from Person p1
+    join Person p2 on p1.email = p2.email and p1.id != p2.id;
